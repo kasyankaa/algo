58e9d6131b0794b9a15d58c3d65766e3ebcbd25b
Christina Kasyan <55959819+kasyankaa@users.noreply.github.com>
2020-10-26 17:39:49 +0200

    Update insertion_sort.py

--- managers/insertion_sort.py
+++ managers/insertion_sort.py
@@ -1,11 +1,8 @@
 class InsertionSort:
     number_of_comparisons = 0
     number_of_swaps = 0
-
     @staticmethod
     def insertion_sort_descending(arr):
-        InsertionSort.number_of_swaps = 0
-        InsertionSort.number_of_comparisons = 0
         for index_of_element in range(1, len(arr)):
             element = arr[index_of_element]
             index_of_another_element = index_of_element - 1
@@ -15,6 +12,13 @@ class InsertionSort:
                 index_of_another_element -= 1
                 InsertionSort.number_of_swaps += 1
             arr[index_of_another_element + 1] = element
+
+
+
+
+
+
+
 
 
 
